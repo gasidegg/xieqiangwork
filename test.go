43c8e1dc88be982c7d6e923e7d74b1473a655711
ytmxk <574546696@qq.com>
2018-04-26 10:23:37 +0800

    sdfs;

--- test.go
+++ test.go
@@ -1,23 +1,23 @@
 package main
 
 import . "fmt"
-
-type Cr struct {
-	radius float64
-}
-
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
 
 
 func main(){
-	var c1 Cr
-	c1.radius = 5
-	Println(c1.getA())
-	Println(g)
-}
-
-var g = 60
-
-func (c Cr) getA() float64 {
-	return 3.14 * c.radius
+	var db , err = sql.Open("mysql", "root:gasidegg@tcp(localhost:3306)/test?charset=utf8")
+	if err != nil {
+        Println(err)
+    }
+    var num , id int64
+    var openid = "s"
+    row , _ := db.Query("SELECT num,id FROM yd_alldaylog where openid like %?%",openid)
+    for row.Next(){
+    	row.Scan(&num,&id)
+		Println(num)
+		Println(id)
+    }
+	
 }
 
